01d41ba91201d1a7a671b6b15c358031f97f9532
andreassef <andreassef02@gmail.com>
2020-07-22 16:55:06 -0400

    Criação das telas do projeto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,25 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header topo">{{ __('Informe seu aniversário') }}</div>
 
-                <div class="card-body">
+                <div class="card-body fundo">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <form action="#" method="POST">
+                        @csrf
+                        <div class="form-group">
+                            <input class="form-control" type="date" name="date">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,144 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            {{-- coluna 1 --}}
+            <div class="col-md-3"> 
+                <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Janeiro</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+            {{-- coluna 2 --}}
+            <div class="col-md-3">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Fevereiro</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- coluna 3 --}}
+            <div class="col-md-3"> 
+                <div class="card text-white topo mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Março</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+            {{-- coluna 4 --}}
+            <div class="col-md-3"> 
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Abril</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- linha 2 --}}
+        <div class="row">
+            {{-- coluna 5 --}}
+            <div class="col-md-3"> 
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Maio</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- coluna 6 --}}
+            <div class="col-md-3">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Junho</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- coluna 7 --}}
+            <div class="col-md-3">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Julho</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- coluna 8 --}}
+            <div class="col-md-3">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Agosto</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- linha 3 --}}
+        <div class="row">
+            {{-- coluna 9 --}}
+            <div class="col-md-3"> 
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Setembro</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- coluna 10 --}}
+            <div class="col-md-3">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Outubro</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- coluna 11 --}}
+            <div class="col-md-3">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Novembro</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+
+            {{-- coluna 12 --}}
+            <div class="col-md-3">
+                <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Dezembro</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Título de Card Success</h5>
+                        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+@endsection
